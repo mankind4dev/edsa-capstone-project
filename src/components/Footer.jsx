4ce2292a608aace032239c3925320aae82d1023b
mankind4dev <ogunmakin120@gmail.com>
2023-08-07 08:01:28 +0100

    CSS improvement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,7 @@ const Footer = () => {
     <footer className='Footer'>
         <div className='sb_footer-copyright'>
           <p>@{new Date().getFullYear()} Gogo<span>Live</span>. All right reserved.</p>
-        </div>
-        <div className='sb_footer-below-links'>
-          <a href="terms"><div><p>Teams $ Conditions</p></div></a>
-          <a href="privacy"><div><p>Privacy</p></div></a>
-          <a href="security"><div><p>Security</p></div></a>
-          <a href="cookie"><div><p>Cookie Declaration</p></div></a>
-        </div>
-      
+        </div>      
     </footer>
   )
 }
